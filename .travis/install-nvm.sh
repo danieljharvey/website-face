45a6fa3d73a9203fafde4c5c48524265ce972b3e
Daniel Harvey <danieljamesharvey@gmail.com>
2019-01-13 19:57:39 +0000

    Tweak nvm script

--- .travis/install-nvm.sh
+++ .travis/install-nvm.sh
@@ -3,4 +3,4 @@
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | sh
 
 export NVM_DIR="${XDG_CONFIG_HOME/:-$HOME/.}nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+# [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
