d6b075ab5929998c4990d50b66b42a45bfb096ff
Daniel Harvey <daniel.harvey@habito.com>
2019-01-17 13:40:32 +0000

    Terrible purescript

--- purescript/src/EffectAndAff.purs
+++ purescript/src/EffectAndAff.purs
@@ -1,10 +1,11 @@
 module EffectAndAff where
 
-import Prelude (Unit, bind, discard, pure, unit)
+import Prelude (Unit, const, bind, discard, pure, unit, ($))
 import Data.Show
 
 import Effect (Effect)
 import Effect.Timer
+import Effect.Console
 
 newtype TimeReturn = TimeReturn TimeoutId
 
@@ -18,3 +19,23 @@ callbackWithDelay :: forall a. a -> (a -> Effect Unit) -> Effect TimeReturn
 callbackWithDelay a cb = do
   timeoutId <- (setTimeout 1000 (cb a))
   pure (TimeReturn timeoutId)
+
+manyCallbacks :: forall a. a -> (a -> Effect Unit) -> Effect Unit
+manyCallbacks a cb
+  = do
+     log "1"
+     _ <- callbackWithDelay a 
+      (\b -> do 
+         log "2" 
+         _ <- callbackWithDelay b 
+          (\c -> do
+              log "3"
+              _ <- cb c
+              pure unit) 
+         pure unit)
+     pure unit
+-- returns
+-- 1
+-- unit
+-- 2
+-- 3
--- purescript/test/Main.purs
+++ purescript/test/Main.purs
@@ -3,11 +3,7 @@ module Test.Main where
 import Prelude
 import Effect (Effect)
 
-import Test.Unit (suite, test, timeout)
 import Test.Unit.Main (runTest)
-import Test.Unit.Assert as Assert
-
-import Effect.Aff (launchAff_)
 
 import Tests.EffectAndAff as EffectAndAff
 
