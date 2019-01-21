f2be2b001d48933cd5fa1b6fb959ea1fe1bbb082
Daniel Harvey <daniel.harvey@habito.com>
2019-01-21 12:56:34 +0000

    purs stuff

--- purescript/src/EffectAndAff.purs
+++ purescript/src/EffectAndAff.purs
@@ -1,11 +1,14 @@
 module EffectAndAff where
 
-import Prelude (Unit, const, bind, discard, pure, unit, ($))
+import Prelude (Unit, bind, discard, pure, unit, ($), mempty)
 import Data.Show
+import Data.Either
 
 import Effect (Effect)
+import Effect.Class (liftEffect)
 import Effect.Timer
 import Effect.Console
+import Effect.Aff
 
 newtype TimeReturn = TimeReturn TimeoutId
 
@@ -23,19 +26,71 @@ callbackWithDelay a cb = do
 manyCallbacks :: forall a. a -> (a -> Effect Unit) -> Effect Unit
 manyCallbacks a cb
   = do
-     log "1"
-     _ <- callbackWithDelay a 
-      (\b -> do 
-         log "2" 
-         _ <- callbackWithDelay b 
-          (\c -> do
-              log "3"
-              _ <- cb c
-              pure unit) 
-         pure unit)
-     pure unit
--- returns
--- 1
--- unit
--- 2
--- 3
+      log "1"
+      _ <- callbackWithDelay a
+        (\b -> do
+          log "2"
+          _ <- callbackWithDelay b
+            (\c -> do
+              log "2"
+              _ <- callbackWithDelay c
+                (\d -> do
+                  log "2"
+                  _ <- callbackWithDelay d
+                    (\e -> do
+                      log "2"
+                      _ <- callbackWithDelay e
+                        (\f -> do
+                          log "2"
+                          _ <- callbackWithDelay f
+                            (\g -> do
+                              log "2"
+                              _ <- callbackWithDelay g
+                                (\h -> do
+                                  log "2"
+                                  _ <- callbackWithDelay h
+                                    (\i -> do
+                                      log "2"
+                                      _ <- callbackWithDelay i
+                                        (\j -> do
+                                          log "3"
+                                          _ <- cb j
+                                          pure unit)
+                                      pure unit)
+                                  pure unit)
+                              pure unit)
+                          pure unit)
+                      pure unit)
+                  pure unit)
+              pure unit)
+          pure unit)
+      pure unit
+
+delayAff :: forall a. a -> Aff a
+delayAff a = makeAff affCallback
+  where
+    affCallback :: (Either Error a -> Effect Unit) -> Effect Canceler
+    affCallback success
+      = do
+          _ <- callbackWithDelay a (\b -> success (Right b))
+          pure mempty
+    
+    makeCanceller :: TimeoutId -> Error -> Aff Unit
+    makeCanceller timer
+      = (\_ -> do
+          liftEffect $ clearTimeout timer
+          pure unit)
+
+go :: forall a. (Show a) => a -> Effect Unit
+go a = do
+  runAff_ (\answer -> log (show answer)) (makeAffs a)
+
+makeAffs :: forall a. a -> Aff a
+makeAffs a = do
+  b <- delayAff a
+  liftEffect $ log "1"
+  c <- delayAff b
+  liftEffect $ log "2"
+  d <- delayAff c
+  liftEffect $ log "3"
+  delayAff d
