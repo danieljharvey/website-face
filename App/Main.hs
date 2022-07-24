16d96b63963efcf114cedc9bd4d15cdeada3b48d
Daniel Harvey <danieljamesharvey@gmail.com>
2022-07-24 13:43:31 +0100

    Rss and atom feeds

--- App/Main.hs
+++ App/Main.hs
@@ -1,90 +1,122 @@
 --------------------------------------------------------------------------------
 {-# LANGUAGE OverloadedStrings #-}
-import           Data.Monoid (mappend, (<>))
-import           Hakyll
 
+import Data.Monoid (mappend, (<>))
+import Hakyll
+
+myFeedConfiguration :: FeedConfiguration
+myFeedConfiguration =
+  FeedConfiguration
+    { feedTitle = "Daniel J Harvey: Internet website",
+      feedDescription = "Writing about computers",
+      feedAuthorName = "Daniel J. Harvey",
+      feedAuthorEmail = "danieljamesharvey@gmail.com",
+      feedRoot = "https://danieljharvey.github.io/"
+    }
 
 --------------------------------------------------------------------------------
 main :: IO ()
 main = hakyll $ do
-    match "images/*" $ do
-        route   idRoute
-        compile copyFileCompiler
-    
-    match "fonts/*" $ do
-        route idRoute
-        compile copyFileCompiler
+  match "images/*" $ do
+    route idRoute
+    compile copyFileCompiler
 
-    match "css/*" $ do
-        route   idRoute
-        compile compressCssCompiler
+  match "fonts/*" $ do
+    route idRoute
+    compile copyFileCompiler
 
-    match "contact.markdown" $ do
-        route   $ setExtension "html"
-        compile $ pandocCompiler
-            >>= loadAndApplyTemplate "templates/default.html" defaultContext
-            >>= relativizeUrls
+  match "css/*" $ do
+    route idRoute
+    compile compressCssCompiler
 
-    -- build up tags
-    tags <- buildTags "posts/*" (fromCapture "tags/*.html")
-    tagsRules tags $ \tag pattern -> do
-       let title = "Posts tagged \"" ++ tag ++ "\""
-       route idRoute
-       compile $ do
-           posts <- recentFirst =<< loadAll pattern
-           let ctx = constField "title" title
-                     `mappend` listField "posts" postCtx (return posts)
-                     `mappend` defaultContext
+  match "contact.markdown" $ do
+    route $ setExtension "html"
+    compile $
+      pandocCompiler
+        >>= loadAndApplyTemplate "templates/default.html" defaultContext
+        >>= relativizeUrls
 
-           makeItem ""
-               >>= loadAndApplyTemplate "templates/tag.html" ctx
-               >>= loadAndApplyTemplate "templates/default.html" ctx
-               >>= relativizeUrls
+  -- build up tags
+  tags <- buildTags "posts/*" (fromCapture "tags/*.html")
+  tagsRules tags $ \tag pat -> do
+    let title = "Posts tagged \"" ++ tag ++ "\""
+    route idRoute
+    compile $ do
+      posts <- recentFirst =<< loadAll pat
+      let ctx =
+            constField "title" title
+              `mappend` listField "posts" postCtx (return posts)
+              `mappend` defaultContext
 
-    match "posts/*" $ do
-        route $ setExtension "html"
-        compile $ pandocCompiler
-            >>= loadAndApplyTemplate "templates/post.html"    (postCtxWithTags tags)
-            >>= loadAndApplyTemplate "templates/default.html" (postCtxWithTags tags)
-            >>= relativizeUrls
+      makeItem ""
+        >>= loadAndApplyTemplate "templates/tag.html" ctx
+        >>= loadAndApplyTemplate "templates/default.html" ctx
+        >>= relativizeUrls
 
-    create ["archive.html"] $ do
-        route idRoute
-        compile $ do
-            posts <- recentFirst =<< loadAll "posts/*"
-            let archiveCtx =
-                    listField "posts" postCtx (return posts) <>
-                    constField "title" "Archives"            <>
-                    defaultContext
+  match "posts/*" $ do
+    route $ setExtension "html"
+    compile $
+      pandocCompiler
+        >>= loadAndApplyTemplate "templates/post.html" (postCtxWithTags tags)
+        >>= saveSnapshot "content"
+        >>= loadAndApplyTemplate "templates/default.html" (postCtxWithTags tags)
+        >>= relativizeUrls
 
-            makeItem ""
-                >>= loadAndApplyTemplate "templates/archive.html" archiveCtx
-                >>= loadAndApplyTemplate "templates/default.html" archiveCtx
-                >>= relativizeUrls
+  create ["archive.html"] $ do
+    route idRoute
+    compile $ do
+      posts <- recentFirst =<< loadAll "posts/*"
+      let archiveCtx =
+            listField "posts" postCtx (return posts)
+              <> constField "title" "Archives"
+              <> defaultContext
 
+      makeItem ""
+        >>= loadAndApplyTemplate "templates/archive.html" archiveCtx
+        >>= loadAndApplyTemplate "templates/default.html" archiveCtx
+        >>= relativizeUrls
 
-    match "index.html" $ do
-        route idRoute
-        compile $ do
-            posts <- recentFirst =<< loadAll "posts/*"
-            let indexCtx =
-                    listField "posts" postCtx (return posts) <>
-                    constField "title" "Internet Website"                <>
-                    defaultContext
+  match "index.html" $ do
+    route idRoute
+    compile $ do
+      posts <- recentFirst =<< loadAll "posts/*"
+      let indexCtx =
+            listField "posts" postCtx (return posts)
+              <> constField "title" "Internet Website"
+              <> defaultContext
 
-            getResourceBody
-                >>= applyAsTemplate indexCtx
-                >>= loadAndApplyTemplate "templates/default.html" indexCtx
-                >>= relativizeUrls
+      getResourceBody
+        >>= applyAsTemplate indexCtx
+        >>= loadAndApplyTemplate "templates/default.html" indexCtx
+        >>= relativizeUrls
 
-    match "templates/*" $ compile templateBodyCompiler
+  match "templates/*" $ compile templateBodyCompiler
 
+  create ["atom.xml"] $ do
+    route idRoute
+    compile $ do
+      let feedCtx =
+            postCtx
+              `mappend` constField "description" "This is the post description"
+
+      posts <- fmap (take 10) . recentFirst =<< loadAll "posts/*"
+      renderAtom myFeedConfiguration feedCtx posts
+
+  create ["rss.xml"] $ do
+    route idRoute
+    compile $ do
+      let feedCtx =
+            postCtx
+              `mappend` constField "description" "This is the post description"
+
+      posts <- fmap (take 10) . recentFirst =<< loadAll "posts/*"
+      renderRss myFeedConfiguration feedCtx posts
 
 --------------------------------------------------------------------------------
 postCtx :: Context String
 postCtx =
-    dateField "date" "%B %e, %Y" <>
-    defaultContext
+  dateField "date" "%B %e, %Y"
+    <> defaultContext
 
 postCtxWithTags :: Tags -> Context String
 postCtxWithTags tags = tagsField "tags" tags `mappend` postCtx
